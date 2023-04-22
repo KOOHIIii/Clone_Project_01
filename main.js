3bac388189fbe05c5c86be514e3473c239eacdf9
Nutthakun.W <95735459+KOOHIIii@users.noreply.github.com>
2023-04-22 18:23:41 +0700

    Try to add function for click btn dropdown and shown dropdown-contet  in javascript file

---
+++ main.js
@@ -0,0 +1,6 @@
+document.getElementById("myCard").addEventListener("click", function () {
+  document.getElementById("dropDown").style.display = "block";
+});
+document.getElementById("myCard").addEventListener("dblclick", function () {
+  document.getElementById("dropDown").style.display = "none";
+});
